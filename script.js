186a7deab22dcdce1da4c2fb4bd1a90f54909ac7
Oona Sofia <Oona.S.Karkkainen@student.laurea.fi>
2026-05-16 19:23:38 +0300

    fetch api

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const API_KEY = "94c3dbc622abfee90c62edf339a49fc4";
-const BASE_URL = "https://ws.audioscrobbler.com/2.0/";
+const url = `https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=Cher&api_key=${API_KEY}&format=json&limit=5`;
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => console.log(data))
+ .catch(error => console.error("Virhe:", error));
