c98ba34c20e96c3b516e45d6ba9452274de4bcc0
Oona Sofia <Oona.S.Karkkainen@student.laurea.fi>
2026-05-29 17:51:10 +0300

    query selector update

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ var API_KEY = "94c3dbc622abfee90c62edf339a49fc4";
 var url = `https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=Cher&api_key=${API_KEY}&format=json&limit=5`;
 
 var albumsDiv = document.querySelector("#albums");
+var artistList = document.querySelector("#artistList");
+const artistTitle = document.querySelector("#artistTitle");
+
+var searchInput = document.querySelector("#artistSearch");
+var searchButton = document.querySelector("#searchButton");
 
 fetch(url)
   .then(response => response.json())
